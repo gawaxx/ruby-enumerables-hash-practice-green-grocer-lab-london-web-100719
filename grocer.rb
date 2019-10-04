fa9483c05cbcec360e695b01e356b930b69be877
jules blanc <jules.blanc@unccub.com>
2019-10-04 01:03:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,17 +11,16 @@ def consolidate_cart(cart)
     end
   }
   return cart_hash
-end 
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
-  # code here
+  
 end
 
 def checkout(cart, coupons)
-  # code here
+  
 end
