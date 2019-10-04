b6eac021bafd2434ceb3946750bebe5c70d3e00f
jules blanc <jules.blanc@unccub.com>
2019-10-04 01:06:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,22 +1,25 @@
 def consolidate_cart(cart)
-  theHash = {}
-  cart.each { |item|
+  cart_hash = {}
+  cart.each do |item|
     name = item.keys[0]
     details = item.values[0]
-    if theHash[name] 
-       theHash[name][:count] += 1 
+    if cart_hash[name] 
+       cart_hash[name][:count] += 1 
      else
-       theHash[name] = details
-       theHash[name][:count] = 1
+       cart_hash[name] = details
+       cart_hash[name][:count] = 1
     end
-  }
+  end
   return cart_hash
-end
+end 
+
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     coupon_item = coupon[:item]
     if cart[coupon_item]
+      # do something to apply the discount
+      
       if coupon[:num] <= cart[coupon_item][:count]
         cart[coupon_item][:count] -= coupon[:num]
         new_key = "#{coupon_item} W/COUPON" 
